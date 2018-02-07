22afada1c422291ac9a2b3be862b5520bfaf79b8
vagrant <vagrant@vagrant.vm>
2018-02-07 02:12:43 +0000

    Remove .vim directory

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+mkdir ~/.vim
+mkdir ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git .vim/bundle/Vundle.vim
 git clone https://github.com/pangloss/vim-javascript.git .vim/bundle/vim-javascript
 
