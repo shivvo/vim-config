05ac9ee5c1b94ffaf74d00eabb5e7612f95d6305
shivvo <shivr2035@gmail.com>
2020-10-11 14:41:10 -0500

    Update setup script for Neovim

--- setup.sh
+++ setup.sh
@@ -1,16 +1,7 @@
 #! /bin/bash
 
-mkdir ~/.vim
-mkdir ~/.vim/bundle
-
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone https://github.com/pangloss/vim-javascript.git ~/.vim/bundle/vim-javascript
-curdir=`pwd`
-git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
-cd ~/.vim/bundle/YouCompleteMe
-git submodule update --init --recursive
-./install.py --clang-completer --js-completer
-cd $curdir
+mkdir -p ~/.config/nvim
+cp init.vim ~/.config/nvim/init.vim
 
 cp vimrc.vimrc ~/.vimrc
 
